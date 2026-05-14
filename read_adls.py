ecdb6da58c502a5bfe24379910bbfef751874368
pdefusco <pauldefusco@gmail.com>
2026-05-14 13:49:23 -0700

    genesis

---
+++ read_adls.py
@@ -0,0 +1,70 @@
+#****************************************************************************
+# (C) Cloudera, Inc. 2020-2026
+#  All rights reserved.
+#
+#  Applicable Open Source License: GNU Affero General Public License v3.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# #  Author(s): Paul de Fusco
+#***************************************************************************/
+
+import os
+import cml.data_v1 as cmldata
+from pyspark.sql import SparkSession
+from pyspark import SparkContext
+
+filePath = os.environ["FILE_PATH"] #e.g. "abfss://<container>@<storage-account>.dfs.core.windows.net/path/to/file.csv"
+tableName = os.environ["TABLE_NAME"] #e.g. "default.myTable"
+
+SparkContext.setSystemProperty('spark.executor.cores', '4')
+SparkContext.setSystemProperty('spark.executor.memory', '8g')
+
+conn = cmldata.get_connection(self.connectionName)
+spark = conn.get_spark_session()
+
+# Read from CSV
+
+dfFromAdls = (
+    spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv(filePath)
+)
+
+dfFromAdls.show()
+
+# Read from Hive Table
+
+df = spark\
+    .table(tableName)
+
+df.show()
